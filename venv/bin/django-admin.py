3fcfe6a1ab5883dfba16fa62857d5e813121a280
silago <silago.nevermind@gmail.com>
2014-02-12 12:47:02 +0900

    removed useless files

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/silago/work/stas/venv/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/lib/python2.7/site-packages/django_thumbs/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-__title__ = 'django-thumbs'
-__version__ = '0.4'
-__author__ = 'Antonio Melé'
